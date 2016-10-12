2cce5c041cd5260888be1e6407859553e2aa1f16
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2016-10-12 09:52:15 -0700

    add nvm and yarn to env.sh

--- bash/env.sh
+++ bash/env.sh
@@ -10,6 +10,13 @@ export PATH="$PATH:$HOME/.rvm/bin"
 # Load RVM into a shell session *as a function*
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
+# Load NVM
+export NVM_DIR="/Users/kshevl/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Load Yarn
+export PATH="$HOME/.yarn/bin:$PATH"
+
 # Turn command line colors on
 export CLICOLOR=1
 
