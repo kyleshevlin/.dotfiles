514cdfce27b8eeae3620fac19d2321f2df70d6e6
Kyle Shevlin <kyle.a.shevlin@gmail.com>
2018-12-21 15:56:17 -0800

    remove function that's no longer used

--- bash/functions.sh
+++ bash/functions.sh
@@ -48,30 +48,6 @@ function mvdirslower () {
   done
 }
 
-# Merge and deploy all branches - Unlikely to be used now
-function mgdep () {
-  if [ $# -eq 0 ]; then
-      branch=$(get_git_branch)
-  else
-    branch=$1
-  fi
-
-  git copl master && echo -e "\033[0;32mMaster checked out and pulled\033[0m"
-  git mg $branch && echo -e "\033[0;32m${branch} merged into master\033[0m"
-  git phom && echo -e "\033[0;32m${branch} pushed to master\033[0m"
-  capp && echo -e "\033[0;32m${branch} deployed to prod via master branch\033[0m"
-  git copl stage && echo -e "\033[0;32mStage checked out and pulled\033[0m"
-  git mg $branch && echo -e "\033[0;32m${branch} merged into stage\033[0m"
-  git phos && echo -e "\033[0;32m${branch} pushed to stage\033[0m"
-  caps && echo -e "\033[0;32m${branch} deployed to stage via stage branch\033[0m"
-  git copl dev && echo -e "\033[0;32mDev checked out and pulled\033[0m"
-  git mg $branch && echo -e "\033[0;32m${branch} merged into dev\033[0m"
-  git phod && echo -e "\033[0;32m${branch} pushed to dev\033[0m"
-  capd && echo -e "\033[0;32m${branch} deployed to dev via dev branch\033[0m"
-  git co master && terminal-notifier -message 'Deploys completed. Master checked out.' -sound 'default'
-}
-alias mede="mgdep"
-
 # Create a new rvm gemset, touch a .rvmrc file, and write to .rvmrc file
 function creatervm () {
   local version_and_name=$1
