1f9e298cfc3d8d24ad1c99da412d77309226cd0a
Justin Levi Winter <justinlevi@users.noreply.github.com>
2019-10-19 12:22:54 -0400

    For mad flavor ;)

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -2,7 +2,7 @@
 PS1='\h:\W \u\$ '
 
 # Full prompt command
-PROMPT_COMMAND='PS1="\n${c_user}\u: ${c_path}\W${c_reset}$(node_prompt)${c_reset}$(git_prompt)\n${c_reset}\$ "'
+PROMPT_COMMAND='PS1="\n${c_user}\u: ${c_path}\W${c_reset}$(node_prompt)${c_reset}$(git_prompt)\n${c_reset}⚡ "'
 
 # For screencasts
-# PROMPT_COMMAND='PS1="\n${c_user}\u: ${c_path}\W\n${c_reset}\$ "'
+# PROMPT_COMMAND='PS1="\n${c_user}\u: ${c_path}\W\n${c_reset}⚡ "'
